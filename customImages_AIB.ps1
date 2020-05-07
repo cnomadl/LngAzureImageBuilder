0902ab9a2502863e5fae18da4b50a072afb5d915
Chris Langford <clangford@lang-ford.uk>
2020-05-07 20:27:23 +0100

    Added PowerShell script

---
+++ customImages_AIB.ps1
@@ -0,0 +1,116 @@
+    param (
+        [cmdletbinding()]      
+        
+        # Destination image resource group
+        [Parameter(Mandatory)]
+        [string]
+        $imageResourceGroup,
+
+        # Location used for replication and the resource group
+        [Parameter(Mandatory)]
+        [string]
+        $location,        
+        
+        # Name of the image to be created
+        [Parameter(Mandatory)]
+        [string]
+        $imageName,
+
+        #Image template name
+        [Parameter(Mandatory)]
+        [string]
+        $imageTemplateName,
+
+        # Distribution properties object name (runOutput), i.e. this gives you the properties of the managed image on completion
+        [Parameter(Mandatory)]
+        [string]
+        $runOutputName
+    )
+
+    # Import Azure Az module
+    Import-Module Az.Accounts
+
+    # Connect to Azure subscription
+    Write-Information -MessageData "Connecting you to your Azure Subscription" -InformationAction Continue
+    Connect-AzAccount
+
+    ## Get exisiting Context
+    $currentAzContext = Get-AzContext
+
+    $subscriptionID=$currentAzContext.Subscription.Id
+
+    # Image resorce group. Create if it does not exist
+    Get-AzResourceGroup -Name $imageResourceGroup -ErrorVariable notPresent -ErrorAction SilentlyContinue
+    if (!$notPresent)
+    {
+        Write-Warning -Message "Resource group already exists."
+    } else {
+        Write-Information -MessageData "Creating Image resource group $imageResourceGroup" -InformationAction Continue
+        New-AzResourceGroup -Name $imageResourceGroup -Location $location
+    }
+
+    ## User identity. Create if the identity does not exist.
+    # Setup role def name. this needs to be unique
+    $timeInt=$(Get-Date -UFormat "%s")
+    $imageRoleDefName="Azure Image Builder Image Def"
+    $identityName="aibIdentity"
+    
+    Install-Module -Name Az.ManagedServiceIdentity
+
+
+    # Check if the identity exists
+    Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+    if (!$notPresent)
+    {
+        Write-Warning -Message "User Identity already exists."
+    } else {
+        Write-Information -MessageData "Creating new user identity $identityName" -InformationAction Continue
+        New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName
+
+        $idenityNameResourceId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).Id
+        $idenityNamePrincipalId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).PrincipalId
+        
+        #Assign permissions for the identity to distribute images
+        $aibRoleImageCreationUrl="https://raw.githubusercontent.com/cnomadl/LngAzureImageBuilder/master/AIB_Security_Roles/aibRoleImageCreation.json"
+        $aibRoleImageCreationPath = "aibRoleImageCreation.json"
+
+        # Download config file
+        Invoke-WebRequest -Uri $aibRoleImageCreationUrl -OutFile $aibRoleImageCreationPath -UseBasicParsing
+
+        ((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $aibRoleImageCreationPath
+        ((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<rgName>', $imageResourceGroup) | Set-Content -Path $aibRoleImageCreationPath
+        ((Get-Content -path $aibRoleImageCreationPath -Raw) -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName) | Set-Content -Path $aibRoleImageCreationPath
+
+        # Create the role definition
+        New-AzRoleDefinition -InputFile  ./aibRoleImageCreation.json
+
+        # Grant role definition to image builder service principle
+        New-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
+    }
+
+    # Additional replication regions
+    $replRegion2
+
+    # Download and coonfigure the image template
+    $templateUrl="https://raw.githubusercontent.com/cnomadl/LngAzureImageBuilder/master/armtemplateWinSIG.json"
+    $templateFilePath = "armTemplateWinSIG.json"
+
+    Invoke-WebRequest -Uri $templateUrl -OutFile $templateFilePath -UseBasicParsing
+
+    ((Get-Content -path $templateFilePath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $templateFilePath
+    ((Get-Content -path $templateFilePath -Raw) -replace '<rgName>',$imageResourceGroup) | Set-Content -Path $templateFilePath
+    ((Get-Content -path $templateFilePath -Raw) -replace '<region>',$location) | Set-Content -Path $templateFilePath
+    ((Get-Content -path $templateFilePath -Raw) -replace '<runOutputName>',$runOutputName) | Set-Content -Path $templateFilePath
+
+    ((Get-Content -path $templateFilePath -Raw) -replace '<imageDefName>',$imageDefName) | Set-Content -Path $templateFilePath
+    ((Get-Content -path $templateFilePath -Raw) -replace '<sharedImageGalName>',$sigGalleryName) | Set-Content -Path $templateFilePath
+    ((Get-Content -path $templateFilePath -Raw) -replace '<region1>',$location) | Set-Content -Path $templateFilePath
+    ((Get-Content -path $templateFilePath -Raw) -replace '<region2>',$replRegion2) | Set-Content -Path $templateFilePath
+
+    ((Get-Content -path $templateFilePath -Raw) -replace '<imgBuilderId>',$idenityNameResourceId) | Set-Content -Path $templateFilePath
+
+    # Submit the template for validation, permissions check and staging
+    New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -api-version "2019-05-001-preview" -imageTemplateName $imageTemplateName -svclocation $location
+
+    # Now we build the image
+    Invoke-AzResourceAction -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2019-05-01-preview" -Action Run -Force
